d39da155a09b600a19d1b408c0a413b486356830
ArmandoP <armandopinzonvera@gmail.com>
2019-11-02 14:31:56 -0500

    Adicion de tabla propiedades y concejos
    se coloco la tabla de propiedades y las fichas de concejos, ademas
    se dejo listo el footer

---
+++ prueba.php
@@ -0,0 +1,12 @@
+<?php
+print"Hello world </br>" ;
+
+/*Variables*/
+
+$nombre = "mandi";
+$correo = 'mandi@ggg';
+$telefono = "29292929";
+$ciudad = 'barcelona';
+
+echo $nombre;
+?>
--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 
+
+
+  var abrirRespiracion = document.getElementById('btn_respirar'),
+      fertilizar = document.getElementById('btn_fertilizar'),
+      abrirMacetas = document.getElementById('btn_macetas'),
+      concejo2v = document.getElementById('concejo2'),
+      concejo3 = document.getElementById('concejo3');
+
+fertilizar.addEventListener('click', function(){
+            concejo2v.classList.add('active');
+            concejo3.classList.remove('active');
+ });
+ abrirMacetas.addEventListener('click', function(){
+      concejo3.classList.add('active');
+});
+ 
+ abrirRespiracion.addEventListener('click', function(){
+      concejo2v.classList.remove('active');
+      concejo3.classList.remove('active');
+});
+
+
+
+
 var abrir_popup = document.getElementById('abrir_popup'),
     overlay = document.getElementById('overlay'),
-    popup = document.getElementsByClassName('popup'),
+   /* popup = document.getElementsByClassName('popup'),*/
     cerrar_popup = document.getElementById('btn_cerrar_popup'),
     submit = document.getElementById('btn_submit');
 
